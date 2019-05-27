c8dc587905a71d9c0e9396015c72ffed0413cfaf
spolex <spolex@msn.com>
2019-05-27 17:54:30 -0400

    eliminar archivos innecesarios

--- pyrestfmri/run_docker_ct.bat
+++
@@ -1,6 +0,0 @@
-docker run ^
- -v %cd%/datos:/home/elekin/data^
- -v %cd%/output:/home/elekin/output^
- -v %cd%/pyrestfmri:/home/elekin/pyrestfmri^
- -v %cd%/pyrestfmri/notebooks:/home/elekin/notebooks^
- -p 8888:8888 -i -t spolex/pyrestfmri:0.1
