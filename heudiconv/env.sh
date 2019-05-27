afd894c3c4ee1cdd8654664b705c3e3c797779ce
spolex <spolex@msn.com>
2019-05-27 17:59:31 -0400

    estabilizando la transformación con heudiconv: parametros, environment...etc

--- heudiconv/env.sh
+++ heudiconv/env.sh
@@ -2,7 +2,7 @@
 INPUT_PATH=$HOME/datos-dicom
 OUTPUT_PATH=$HOME/datos-niix-test
 CODE_PATH=$HOME/docker-pyrestfmri/heudiconv/code
-SUBJECTS_FILE_PATH=$HOME/docker-pyrestfmri/subjs.txt
+SUBJECTS_FILE_PATH=$HOME/docker-pyrestfmri/deleted_subjs.txt
 #SUBJS="$(ls "$INPUT_PATH" | grep T0* | tr '\n' ' ')"
 SUBJS="$(cat $SUBJECTS_FILE_PATH)"
 #CONVERSOR=none
