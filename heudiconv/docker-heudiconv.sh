cb53cf0754d2fd2eaa010d96ecf82ce82da34d10
Ubuntu <hadoop@swarm.mng3>
2019-05-27 20:38:11 +0000

    modificar .gitigore para no empujar datos parametrizar en una variable la localización del script de heudiconv

--- heudiconv/docker-heudiconv.sh
+++ heudiconv/docker-heudiconv.sh
@@ -1,5 +1,9 @@
+source heudiconv/env.sh
+echo "$OUTPUT_PATH"
+echo "$INPUT_PATH"
+echo "$SUBJS"
 
-docker run --rm -it -v $INPUT_PATH:/data:ro \
--v $OUTPUT_PATH:/output nipy/heudiconv:latest \
+docker run --rm -d -v "$INPUT_PATH":/data:ro \
+-v "$OUTPUT_PATH":/output nipy/heudiconv:latest \
 -d /data/{subject}/*/DICOM/*/*/* -s $SUBJS --ses 1 \
--f /output/code/convertall_elekin.py -c dcm2niix -o /output --overwrite
+-f /output/code/convertall_elekin.py -c $CONVERSOR -o /output --overwrite
--- heudiconv/env.sh
+++ heudiconv/env.sh
@@ -1,4 +1,9 @@
 #environment variables
-INPUT_PATH=/var/lib/docker/volumes/heudiconv_datos-dicom/_data/
-OUTPUT_PATH=/var/lib/docker/volumes/heudiconv_output/_data
-SUBJS=T004 T006
+INPUT_PATH=$HOME/datos-dicom
+OUTPUT_PATH=./heudiconv/output
+CODE_PATH=./heudiconv/code
+SUBJECTS_FILE_PATH=/home/hadoop/grametheus/docker-pyrestfmri/deleted_subjs.txt
+#SUBJS="$(ls "$INPUT_PATH" | grep T0* | tr '\n' ' ')"
+SUBJS="$(cat $SUBJECTS_FILE_PATH)"
+CONVERSOR=none
+#CONVERSOR=dcm2niix
