b51ec25477bb2454876956df81e89584ace3c881
spolex <spolex@msn.com>
2019-06-02 04:39:23 +0000

    adjusting docker container for preprocessing

--- pyrestfmri/build_docker_image.sh
+++ pyrestfmri/build_docker_image.sh
@@ -1,4 +1,4 @@
-docker build -t spolex/pyrestfmri:0.1 .
-::docker tag pyrestfmri:elekin pyrestfmri/elekin:latest
-::remove the old name
-::docker rmi pyrestfmri:elekin
+docker build -t spolex/pyrestfmri:0.1 $1 
+
+
+
--- pyrestfmri/generate_docker.sh
+++ pyrestfmri/generate_docker.sh
@@ -2,18 +2,19 @@ docker run --rm kaczmarj/neurodocker:master generate docker --base neurodebian:s
  --label maintainer="Inigo Sanchez <jisanchez003@ehu.es>"\
  --pkg-manager apt\
  --install gcc g++ graphviz tree vim nano git octave\
-# --ants version=2.2.0\
- --fsl version=6.0.1\
+ --fsl version=5.0.8\
+ --ants version=2.3.1\
+ --user elekin\
  --miniconda env_name=elekin\
-	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy spyder dcm2niix"\
+	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy scikit-learn"\
 	pip_install="https://github.com/nipy/nipype/tarball/master https://github.com/INCF/pybids/tarball/master nilearn "\
 	create_env="elekin"\
 	activate=true\
  --run-bash "source activate elekin"\
  --run "mkdir -p ~/.jupyter && echo c.NotebookApp.ip = \'0.0.0.0\' > ~/.jupyter/jupyter_notebook_config.py"\
  --expose 8888 8888\
- --volume $HOME/datos-dicom:$HOME/datos-dicom\
- --volume $HOME/pyrestfmri:$HOME/pyrestfmri\
- --volume $HOME/preproc:$HOME/preproc\
- --workdir $HOMEY/pyrestfmri\
- --cmd python preprocess.py -c conf/config_test.json
+ --volume /home/elekin/datos-dicom\
+ --volume /home/elekin/pyrestfmri\
+ --volume /home/elekin/preproc\
+ --workdir /home/elekin/pyrestfmri\
+ --cmd "/home/elekin/pyrestfmri/preprocess.py","-c","/home/elekin/pyrestfmri/conf/config_test.json" 
