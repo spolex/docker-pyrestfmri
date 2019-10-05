a199543d492369da1e1533f15a523d43aaeeed34
spolex <spolex@msn.com>
2019-10-05 09:12:06 -0400

    crear la generacion del contenedor para ejecutar la fase de preprocesado

--- pyrestfmri/build_docker_image.sh
+++ pyrestfmri/build_docker_image.sh
@@ -1,7 +1,3 @@
-docker build -t spolex/pyrestfmri:0.1 $1 
-<<<<<<< HEAD
-=======
+docker build -t spolex/pyrestfmri:0.1 $1
 
 
-
->>>>>>> b51ec25477bb2454876956df81e89584ace3c881
--- pyrestfmri/generate_docker.sh
+++ pyrestfmri/generate_docker.sh
@@ -2,18 +2,19 @@ docker run --rm kaczmarj/neurodocker:master generate docker --base neurodebian:s
  --label maintainer="Inigo Sanchez <jisanchez003@ehu.es>"\
  --pkg-manager apt\
  --install gcc g++ graphviz tree vim nano git octave\
- --fsl version=5.0.10\
+ --fsl version=5.0.8\
  --ants version=2.3.1\
+ --user elekin\
  --miniconda env_name=elekin\
-	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy dcm2niix scikit-learn"\
-	pip_install="https://github.com/nipy/nipype/tarball/master https://github.com/INCF/pybids/tarball/master nilearn "\
+	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy scikit-learn"\
+	pip_install="https://github.com/nipy/nipype/tarball/master https://github.com/INCF/pybids/tarball/master nilearn tensorflow Keras"\
 	create_env="elekin"\
 	activate=true\
- --user elekin\
  --run-bash "source activate elekin"\
  --run "mkdir -p ~/.jupyter && echo c.NotebookApp.ip = \'0.0.0.0\' > ~/.jupyter/jupyter_notebook_config.py"\
  --expose 8888 8888\
  --volume /home/elekin/datos-dicom\
  --volume /home/elekin/pyrestfmri\
+ --volume /home/elekin/preproc\
  --workdir /home/elekin/pyrestfmri\
- --cmd "jupyter-notebook"
+ --cmd "/home/elekin/pyrestfmri/preprocess.py","-c","/home/elekin/pyrestfmri/conf/config_test.json" 
