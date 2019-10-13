0adcbdfa4d2b062b360800cc0f2a6e4c2ee963a5
Iñigo <jisanchez003@ikasle.ehu.es>
2019-10-13 09:05:10 -0400

    add niwidgets to generate docker

--- pyrestfmri/generate_docker.sh
+++ pyrestfmri/generate_docker.sh
@@ -9,7 +9,7 @@ docker run --rm kaczmarj/neurodocker:master generate docker --base neurodebian:s
  --run "mkdir ~/.conda"\
  --miniconda create_env=elekin\
 	conda_install="traits jupyter jupyterlab matplotlib nibabel pip nitime numpy scikit-learn networkx scipy"\
-	pip_install="https://github.com/nipy/nipype/tarball/master nilearn"\
+	pip_install="https://github.com/nipy/nipype/tarball/master nilearn niwidgets"\
  	activate=true\
  --run "mkdir -p ~/.jupyter && echo c.NotebookApp.ip = \'0.0.0.0\' > ~/.jupyter/jupyter_notebook_config.py"\
  --expose 8888 8888\
