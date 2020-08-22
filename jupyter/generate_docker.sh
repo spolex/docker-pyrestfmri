0c68ddcd782c89397c23c4b0afa1ad97a72c6863
Iñigo <jisanchez003@ikasle.ehu.es>
2020-08-21 20:41:14 -0400

    change user parameter position
    Issues with opt environment privileges

--- jupyter/generate_docker.sh
+++ jupyter/generate_docker.sh
@@ -5,12 +5,12 @@ docker run --rm kaczmarj/neurodocker:master generate docker --base neurodebian:s
  --install gcc g++ graphviz tree vim nano git octave\
  --fsl version=5.0.8\
  --ants version=2.3.1\
+ --user elekin\
  --miniconda env_name=elekin\
-	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy dcm2niix scikit-learn tensorflow mkl pygpu pandas"\
-	pip_install="https://github.com/nipy/nipype/tarball/master https://github.com/INCF/pybids/tarball/master nilearn git+git://github.com/nipy/niwidgets/ nxpd seaborn"\
+	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy dcm2niix scikit-learn tensorflow=2.2.0 mkl pygpu pandas"\
+	pip_install="https://github.com/nipy/nipype/tarball/master https://github.com/INCF/pybids/tarball/master nilearn git+git://github.com/nipy/niwidgets/ nxpd seaborn git+https://github.com/tensorflow/docs tables"\
 	create_env="elekin"\
 	activate=true\
- --user elekin\
  --run-bash "source activate elekin"\
  --run "mkdir -p ~/.jupyter && echo c.NotebookApp.ip = \'0.0.0.0\' > ~/.jupyter/jupyter_notebook_config.py"\
  --expose 8888 8888\
