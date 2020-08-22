91facda6ebe442623901d9c3d3862308b2f6895e
Iñigo <jisanchez003@ikasle.ehu.es>
2020-08-21 22:21:02 -0400

    user id in docker run and add tensorboard ports

--- jupyter/generate_docker.sh
+++ jupyter/generate_docker.sh
@@ -2,19 +2,20 @@
 docker run --rm kaczmarj/neurodocker:master generate docker --base neurodebian:stretch-non-free\
  --label maintainer="Inigo Sanchez <jisanchez003@ehu.es>"\
  --pkg-manager apt\
- --install gcc g++ graphviz tree vim nano git octave\
+ --install gcc g++ graphviz tree vim git htop\
  --fsl version=5.0.8\
  --ants version=2.3.1\
  --user elekin\
+ --user root\
+ --run-bash "usermod -u 1001 elekin"\
+ --user elekin\
  --miniconda env_name=elekin\
-	conda_install="python=3.6 traits jupyter jupyterlab matplotlib nibabel pip nitime numpy dcm2niix scikit-learn tensorflow=2.2.0 mkl pygpu pandas"\
+	conda_install="python=3.6 traits jupyter matplotlib nibabel pip nitime numpy dcm2niix scikit-learn tensorflow=2.2.0 mkl pygpu pandas"\
 	pip_install="https://github.com/nipy/nipype/tarball/master https://github.com/INCF/pybids/tarball/master nilearn git+git://github.com/nipy/niwidgets/ nxpd seaborn git+https://github.com/tensorflow/docs tables"\
 	create_env="elekin"\
 	activate=true\
+ --run-bash "usermod -u 1001 elekin"\
  --run-bash "source activate elekin"\
  --run "mkdir -p ~/.jupyter && echo c.NotebookApp.ip = \'0.0.0.0\' > ~/.jupyter/jupyter_notebook_config.py"\
  --expose 8888 8888\
- --volume /home/elekin/datos-dicom\
- --volume /home/elekin/pyrestfmri\
- --workdir /home/elekin/pyrestfmri\
  --cmd "jupyter-notebook"
